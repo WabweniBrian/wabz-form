5448c71cfa7c27adcf83a8102bc1ef16a09135b2
Wabweni Brian <115115387+WabweniBrian@users.noreply.github.com>
2024-02-11 13:34:52 +0300

    Updates

--- components/logo.tsx
+++ components/logo.tsx
@@ -7,7 +7,7 @@ const Logo = () => {
       href="/"
       className="font-bold text-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 text-transparent bg-clip-text"
     >
-      PageForm
+      WabForms
     </Link>
   );
 };
